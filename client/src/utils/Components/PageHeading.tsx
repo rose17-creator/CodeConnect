734878958e6760f24d79f3de32caa67186789c04
rose17-creator <58398483+rose17-creator@users.noreply.github.com>
2024-06-17 11:50:13 +0530

    Delete client/src/utils/Components directory

--- client/src/utils/Components/PageHeading.tsx
+++
@@ -1,9 +0,0 @@
-
-const PageHeading = ({ heading }) => {
-
-    return (
-        <h2 className='font-bold text-emerald-500 text-[32px] capitalize ' >{heading}</h2>
-    )
-}
-
-export default PageHeading;
